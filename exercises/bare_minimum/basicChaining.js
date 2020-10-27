daba2e65390ca897229f48261458822e75414754
Thomas Brannan <th.c.brannan@gmail.com>
2020-10-27 12:48:37 -0700

    With help from Kimberly, passed all tests for fetchProfileAndWriteToFile()

--- exercises/bare_minimum/basicChaining.js
+++ exercises/bare_minimum/basicChaining.js
@@ -11,10 +11,22 @@
 var fs = require('fs');
 var Promise = require('bluebird');
 
-
+var { pluckFirstLineFromFileAsync } = require('./promiseConstructor.js');
+var { getGitHubProfileAsync } = require('./promisification.js');
 
 var fetchProfileAndWriteToFile = function(readFilePath, writeFilePath) {
-  // TODO
+  /**
+   * use pluckFirstLineFromFileAsync() to read the gitHub username
+   * then, obtain that user's profile
+   * then, write it
+   * if there was an error, handle it
+   */
+  return pluckFirstLineFromFileAsync(readFilePath)
+    .then((user) => getGitHubProfileAsync(user))
+    .then((profile) => fs.writeFileSync(writeFilePath, JSON.stringify(profile)))
+    .catch ((error) => {
+      console.log(`Error in exercises/basicChaining.js::fetchProfileAndWriteToFile\n${JSON.stringify(error)}`);
+    });
 };
 
 // Export these functions so we can test them
